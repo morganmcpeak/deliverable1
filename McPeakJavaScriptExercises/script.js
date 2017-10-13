83f6323725218d7f8bdf9142f03b918c231f84d7
morganmcpeak <32708910+morganmcpeak@users.noreply.github.com>
2017-10-12 23:40:34 -0400

    Add files via upload

---
+++ McPeakJavaScriptExercises/script.js
@@ -0,0 +1,51 @@
+
+//exercise 1
+
+var user = prompt ("pleasee enter your number");
+
+var total = 0;
+for (var num = 1; num < 11; num++) {
+	total + total + num;
+}
+console.log('the total is: ' + total); 
+
+//exercise 2
+
+var text =["yes", "dog", "yes", "cat", "no", "dogcat"]
+var x = 0;
+do {
+	var user = prompt("Do you want to play?");
+	x++;
+}
+while (x<=text.length);
+do {
+	var user = prompt("Enter a word.");
+	x++;
+}
+while (x<=text.length);
+do {
+	var user = prompt("Do you want to play again?");
+	x++;
+}
+while (x<=text.length);
+do {
+	var user = prompt("Enter a word.");
+	x++;
+}
+while (x<=text.length);
+do {
+	var user = prompt("Do you want to play again?");
+	x++;
+}
+while (x<=text.length);
+x++;
+console.log(dogcat);
+
+
+//exercise 3
+var text = "Do you want to print your name?"
+
+
+
+
+
